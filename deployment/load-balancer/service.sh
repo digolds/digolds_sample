d2b0c3c4c16c4aac2b97fe697001874d034f903d
caroline <ynxuan1990love@sina.cn>
2020-11-11 23:25:54 +0800

    update tar command

--- deployment/load-balancer/service.sh
+++ deployment/load-balancer/service.sh
@@ -6,3 +6,6 @@ yum -y install nginx
 mkdir /srv/digolds
 groupadd www-data
 adduser -g www-data www-data
+
+curl -o /tmp/p.tar.gz https://github.com/digolds/digolds_sample/archive/v0.0.1.tar.gz
+tar xf /tmp/original.tar.gz -C /srv/digolds
