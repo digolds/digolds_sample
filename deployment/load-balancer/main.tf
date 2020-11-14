a900d7a97c44ed0c9e4db68b6fd748c4872f50b7
caroline <ynxuan1990love@sina.cn>
2020-11-14 21:01:20 +0800

    provision load balance

--- deployment/load-balancer/main.tf
+++ deployment/load-balancer/main.tf
@@ -9,6 +9,11 @@ provider "aws" {
 
 module "load_balance" {
   source       = "github.com/2cloudlab/module_load_balancer//modules/load_balancer?ref=v0.0.1"
+  download_url = "https://github.com/digolds/digolds_sample/archive/v0.0.1.tar.gz"
+  package_base_dir         = "digolds_sample-0.0.1"
+  app_dir = "personal-blog"
+  envs     = ["USER_NAME=slz", "PASSWORD=abc", "TABLE_NAME=personal-articles-table", "INDEX_NAME=ContentGlobalIndex"]
+  wsgi_app = "wsgiapp:wsgi_app"
 }
 
 output "alb_dns_name" {
